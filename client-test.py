c4b2932435e4ebdfabd23056c36060b506d9e1a6
sasumaki <saus.makinen@gmail.com>
2020-11-03 15:48:21 +0200

    zz

--- client-test.py
+++ client-test.py
@@ -2,29 +2,31 @@ from seldon_core.seldon_client import SeldonClient
 import numpy as np
 import matplotlib.pyplot as plt
 import json
-from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow_datasets as tfds
 import random
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
+ds = tfds.load(name="mnist", split="test", as_supervised=True)
 
-X_train = np.vstack([img.reshape(-1,) for img in mnist.train.images])
-y_train = mnist.train.labels
-
-X_test = np.vstack([img.reshape(-1,) for img in mnist.test.images])
-y_test = mnist.test.labels
 
 sc = SeldonClient(deployment_name="mnist-model", namespace="seldon-system", gateway_endpoint="localhost:8081", gateway="istio")
 print(sc.config)
-for i in range(0, 1000):
-  data = random.choice(X_test)
+test_size = 100
+corrects = 0
+data = ds.take(test_size).cache()
+for image, label in data:
+
   # plt.imshow(data.reshape(28,28))
   # plt.colorbar()
   # plt.show()
-  r = sc.predict(data=data, gateway="istio",transport="rest")
+  r = sc.predict(data=np.array(image), gateway="istio",transport="rest")
   print(r.msg)
   assert(r.success==True)
 
   res = r.response['data']['tensor']['values']
   print(res)
+  prediction = int(np.argmax(np.array(res).squeeze(), axis=0))
+  print("predicted: ", prediction, "Truth: ", int(label))
+  if prediction == int(label):
+    corrects = corrects + 1
 
-  print(int(np.argmax(np.array(res).squeeze(), axis=0)))
+assert(corrects/test_size > 0.9)
