d0ffebac4796fea23900f371218d96b9e04a5b06
sasumaki <saus.makinen@gmail.com>
2020-11-03 16:14:00 +0200

    ads

--- client-test.py
+++ client-test.py
@@ -4,28 +4,29 @@ import json
 import tensorflow_datasets as tfds
 import random
 
-ds = tfds.load(name="mnist", split="test", as_supervised=True)
+def test():
+  ds = tfds.load(name="mnist", split="test", as_supervised=True)
 
 
-sc = SeldonClient(deployment_name="mnist-model", namespace="seldon-system", gateway_endpoint="localhost:8081", gateway="istio")
-print(sc.config)
-test_size = 100
-corrects = 0
-data = ds.take(test_size).cache()
-for image, label in data:
+  sc = SeldonClient(deployment_name="mnist-model", namespace="seldon-system", gateway_endpoint="localhost:8081", gateway="istio")
+  print(sc.config)
+  test_size = 100
+  corrects = 0
+  data = ds.take(test_size).cache()
+  for image, label in data:
 
-  # plt.imshow(data.reshape(28,28))
-  # plt.colorbar()
-  # plt.show()
-  r = sc.predict(data=np.array(image), gateway="istio",transport="rest")
-  print(r.msg)
-  assert(r.success==True)
+    # plt.imshow(data.reshape(28,28))
+    # plt.colorbar()
+    # plt.show()
+    r = sc.predict(data=np.array(image), gateway="istio",transport="rest")
+    print(r.msg)
+    assert(r.success==True)
 
-  res = r.response['data']['tensor']['values']
-  print(res)
-  prediction = int(np.argmax(np.array(res).squeeze(), axis=0))
-  print("predicted: ", prediction, "Truth: ", int(label))
-  if prediction == int(label):
-    corrects = corrects + 1
+    res = r.response['data']['tensor']['values']
+    print(res)
+    prediction = int(np.argmax(np.array(res).squeeze(), axis=0))
+    print("predicted: ", prediction, "Truth: ", int(label))
+    if prediction == int(label):
+      corrects = corrects + 1
 
-assert(corrects/test_size > 0.9)
+  assert(corrects/test_size > 0.9)
