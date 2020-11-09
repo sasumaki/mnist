9f48a2a973704258ef70df7195c68bb23c13d5bc
sasumaki <saus.makinen@gmail.com>
2020-11-09 17:49:38 +0200

    latest seldon-core and crazy stuff

--- Mnist.py
+++ Mnist.py
@@ -13,7 +13,8 @@ from urllib.parse import urlparse
 from seldon_core.utils import getenv
 import onnxruntime as rt
 import time
-
+# from minio import Minio
+# from minio.error import ResponseError
 logger = logging.getLogger(__name__)
 
 class Mnist(SeldonComponent):
@@ -23,9 +24,26 @@ class Mnist(SeldonComponent):
     self.model_uri = model_uri
     self.method = method
     self.ready = False
+
+
+    # s3Client = Minio(
+    #     's3.amazonaws.com',
+    #     access_key=os.getenv("AWS_ACCESS_KEY_ID", ""),
+    #     secret_key=os.getenv("AWS_SECRET_ACCESS_KEY", ""),
+    #     secure=True,
+    # )
+    model_file = os.path.join(self.model_uri, "model.onnx")
     
-    model_file = os.path.join(seldon_core.Storage.download(self.model_uri), "model.onnx")
+    # try:
+    # response = minio.get_object('foo', 'bar')
+    # finally:
+    #   response.close()
+    #   response.release_conn()
+
     self._model = model_file
+    self.session = rt.InferenceSession(self._model, None)
+    self.input_name = session.get_inputs()[0].name
+    self.output_name = session.get_outputs()[0].name
     self.ready = True
     print("init and model loading done!!!")
 
@@ -39,11 +57,9 @@ class Mnist(SeldonComponent):
   def predict(self, X, features_names):
     start_time = time.time()
     X = np.reshape(X, (1,1,28,28))
-    session = rt.InferenceSession(self._model, None)
-    input_name = session.get_inputs()[0].name
-    output_name = session.get_outputs()[0].name
+    
 
-    res = session.run([output_name], {input_name: X.astype('float32')})
+    res = self.session.run([self.output_name], {self.input_name: X.astype('float32')})
 
     runtime_metrics = {"type": "TIMER", "key": "prediction_time", "value": (time.time() - start_time) * 1000}
 
@@ -57,6 +73,6 @@ class Mnist(SeldonComponent):
         {"type":"GAUGE","key":"mygauge","value":100}, # a gauge which will be set to given value
         {"type":"TIMER","key":"mytimer","value":20.2}, # a timer which will add sum and count metrics - assumed millisecs
       ]
-      
+
   def tags(self,X):
     return {"model_uri": self.model_uri}
